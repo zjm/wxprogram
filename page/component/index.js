6a8b3ae1bac6af57155ddeb487534911db04c201
zjm <bzzhi@qq.com>
2018-09-08 17:30:59 +0800

    change color

--- page/component/index.js
+++ page/component/index.js
@@ -5,11 +5,31 @@ Page({
       //'/image/b2.jpg',
       '/image/b3.jpg'
     ],
-
-    imageSrc: '/image/b3.jpg',
-   
-    //interval: 3000,
-   // duration: 800,
-  }
+    // 最大字符数
+    maxTextLen: 40,
+    // 默认长度
+    textLen: 0
+  },
+  getWords(e) {
+    let page = this;
+    // 设置最大字符串长度(为-1时,则不限制)
+    let maxTextLen = page.data.maxTextLen;
+    // 文本长度
+    let textLen = e.detail.value.length;
+    page.setData({
+      maxTextLen: maxTextLen,
+      textLen: textLen
+    });
+    if (textLen == 40) {
+      page.setData({
+        color: "red"
+      });
+    } else {
+      page.setData({
+        color: "black"
+      });
+    }
+  },
+  
 })
 
